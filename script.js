996ea1485a835950a8b45c54593115fab7ec9851
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2022-07-11 20:16:10 -0600

    declared generate function for later use

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+};
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,7 +12,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
