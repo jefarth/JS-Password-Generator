7874b832a613dc8d6d7582a6491f55963b7717d4
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2022-07-20 01:03:25 -0600

    created a function to ask the user how long they want the password to be

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,27 @@ const uppercase = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
 const number = `1234567890`;
 const character = `!@#$%^&*()_-+={}[];:'"~<,>.?/|`;
 
+function checkLength(passwordLength) {
+  passwordLength = prompt(`Please enter how many characters you want your password to be. \n(Enter a number between 8 and 64)`);
+
+  if (passwordLength === null) {
+    return
+  } else if (passwordLength < 8) {
+    alert("Password must be at least 8 characters long.");
+    checkLength();
+  } else if (passwordLength > 64) {
+    alert("Password must be no more than 64 characters long.");
+    checkLength();
+  } else if (isNaN(passwordLength)) {
+    alert("Please enter a number to define the password length.");
+    checkLength();
+  } else {
+    alert(`Password length set at ${passwordLength} characters. Please follow the upcoming prompts to choose specific criteria.`);
+  }
+  console.log(passwordLength);
+  return passwordLength;
+}
+
 function checkUpperCase(checkUpper) {
   checkUpper = prompt(`Do you want to use upper case letters? \n(Enter Yes or No)`);
   checkUpper = checkUpper.toLowerCase();
@@ -22,11 +43,13 @@ function checkUpperCase(checkUpper) {
     alert(`Please only enter yes or no.`);
     checkUpperCase();
   }
+  console.log(checkUpper);
   return checkUpper;
 }
 
 function generatePassword() {
- checkUpperCase();
+  checkLength();
+  checkUpperCase();
 };
 
 // Write password to the #password input
