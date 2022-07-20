56607e8a3c5447d483549f7ef7ce3e2e1c95641d
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2022-07-20 00:14:57 -0600

    updated var declarations to let declarations

--- assets/script.js
+++ assets/script.js
@@ -32,8 +32,8 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 };
